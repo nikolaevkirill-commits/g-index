950fae44d13bdeec2b1596ccc1944c39c1415454
nikolaevkirill-commits <nikolaev.kirill@gmail.com>
2026-04-11 22:49:08 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
    - Everything else → Network-First, no cache
 */
 
-const SHELL_CACHE  = 'g-index-shell-v70-8b';
+const SHELL_CACHE  = 'g-index-shell-v70-8d';
 const DATA_CACHE   = 'g-index-data-v1';
 const DATA_TTL_MS  = 1 * 60 * 60 * 1000; // 1 hour (Dst оновлюється кожну 1h)
 
