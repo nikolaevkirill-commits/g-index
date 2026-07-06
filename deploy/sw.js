042fe15e1662d07c56c8b9c96e9b418f3402ff82
nikolaevkirill-commits <nikolaev.kirill@gmail.com>
2026-07-06 18:04:32 +0300

    Add files via upload

--- deploy/sw.js
+++ deploy/sw.js
@@ -607,8 +607,8 @@
 //   3. backtest.html додано до SHELL_FILES.
 //   4. cache.put awaited перед SW_FRESH_DATA notify (race fix).
 
-const SHELL_CACHE = 'g-index-shell-v88-8-51-fp128';
-const DATA_CACHE = 'g-index-data-v88-8-51-fp128';
+const SHELL_CACHE = 'g-index-shell-v88-8-55-fp129';
+const DATA_CACHE = 'g-index-data-v88-8-55-fp129';
 
 const SHELL_FILES = [
   './manifest.json',
